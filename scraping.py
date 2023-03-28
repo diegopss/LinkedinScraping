c32ebdb814291602c051cd117265f17887ee635a
Diego passos <die-gops@hotmail.com>
2023-03-27 21:50:24 -0300

    scraping de alguns atributos completos(projeto ainda não finalizado)

--- scraping.py
+++ scraping.py
@@ -1,3 +1,6 @@
+import os
+from datetime import datetime
+
 import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -15,29 +18,12 @@ driver.get(url)
 
 # Espera até que todos os elementos sejam carregados (pode ser necessário ajustar o tempo de espera)
 driver.implicitly_wait(10)
+for i in range(1):
+    # Simula a rolagem da página até o final
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
 
-time.sleep(3)
 
-# Simula a rolagem da página até o final
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-# Espera um pouco para a página carregar novamente
-time.sleep(3)
-# Simula a rolagem da página até o final
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-# Espera um pouco para a página carregar novamente
-time.sleep(3)
-# Simula a rolagem da página até o final
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-# Espera um pouco para a página carregar novamente
-time.sleep(3)
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-# Espera um pouco para a página carregar novamente
-time.sleep(3)
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 # Extrai o HTML da página carregada
 html = driver.page_source
 
@@ -45,18 +31,42 @@ html = driver.page_source
 soup = BeautifulSoup(html, "html.parser")
 
 conteudos = soup.find_all('a', href= re.compile('/jobs/view'))
-urlVagas = []
-tam = 0
+dados = {'urlVaga': '', 'nomeVaga': '','nomeEmpresa': '' , 'urlEmpresa': '', 'modeloContratacao': '', 'tipoContratacao': '', 'nivelExp': '',
+         'numeroCandidaturas': '', 'dataPostagem': '', 'horarioScraping': '', 'numeroFuncionarios': '', 'localSede': '', 'urlCandidatura': ''}
+
 for conteudo in conteudos:
-    urlVagas.append(conteudo)
+    urlVaga = conteudo['href']
 
-#for vaga in soup.find_all('a', href=True):
- #   if '/jobs/view' in vaga['href']:
-  #      urlVagas.append(vaga['href'])
+    driver.get(urlVaga)
+    driver.implicitly_wait(10)
+    html2 = driver.page_source
+    soup2 = BeautifulSoup(html2, "html.parser")
+    time.sleep(2)
 
-for vaga in urlVagas:
-    print(vaga['href'])
+    horarioScraping = datetime.now()
+    horarioScraping = horarioScraping.strftime("%d-%m-%Y %H:%M:%S")
+    urlVaga = conteudo['href']
+    nomeVaga = soup2.find('h1', class_="top-card-layout__title")
+    nomeEmpresa = soup2.find('a', class_="topcard__org-name-link")
+    urlEmpresa = soup2.find('a', class_="topcard__org-name-link")
+    tipoContratacao = soup2.find_all('span', class_= "description__job-criteria-text")
+    nivelExp = soup2.find('span', class_= "description__job-criteria-text")
+    numeroCandidaturas = soup2.find('span', class_= "num-applicants__caption")
+    dataPostagem = soup2.find('span', class_= "posted-time-ago__text")
+    #numeroFuncionarios =
 
-print(len(urlVagas))
+    dados['urlVaga'] = urlVaga
+    dados['nomeVaga'] = nomeVaga.string.strip() if nomeVaga is not None else "Not found"
+    dados['nomeEmpresa'] = nomeEmpresa.string.strip() if nomeEmpresa is not None else "Not found"
+    dados['urlEmpresa'] = urlEmpresa.get('href') if urlEmpresa is not None else "Not found"
+    dados['tipoContratacao'] = tipoContratacao[1].string.strip() if len(tipoContratacao) >= 1 else "Not found"
+    dados['nivelExp'] = nivelExp.string.strip() if nivelExp is not None else "Not found"
+    dados['numeroCandidaturas'] = numeroCandidaturas.string.strip() if numeroCandidaturas is not None else "Not found"
+    dados['dataPostagem'] = dataPostagem.string.strip() if dataPostagem is not None else "Not found"
+    dados['horarioScraping'] = horarioScraping
+    #dados['numeroFuncionarios'] =
+    print(dados['horarioScraping'])
+    df = pd.DataFrame.from_dict(dados, orient='index').T
+    df.to_csv('dados.csv', index=False, mode='a', header=not os.path.isfile('dados.csv'))
 
-print(len(urlVagas))
+
